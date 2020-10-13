52cccb384d20c9d4a1d609d8077e76ea0b6371f1
= <brennocesar@live.com>
2020-10-13 13:00:22 -0400

    Landing update

--- src/components/layout/Navbar/NavbarComponent.js
+++ src/components/layout/Navbar/NavbarComponent.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
-import { Nav, Navbar, Button, Image } from 'react-bootstrap';
+import { Nav, Navbar, Button, Image, Modal, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Brand from '../../../assets/images/Brand.png';
 
@@ -20,8 +21,39 @@ const SignUpBtn = styled(LoginBtn)`
 `;
 
 const NavbarComponent = () => {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
 	return (
-		<div>
+		<>
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Login</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form>
+						<Form.Group controlId='formBasicEmail'>
+							<Form.Label>Email:</Form.Label>
+							<Form.Control type='email' placeholder='example@user.com' />
+						</Form.Group>
+
+						<Form.Group controlId='formBasicPassword'>
+							<Form.Label>Password:</Form.Label>
+							<Form.Control type='password' placeholder='Password' />
+						</Form.Group>
+					</Form>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant='secondary' onClick={handleClose}>
+						Close
+					</Button>
+					<Button variant='primary' onClick={handleClose}>
+						Login
+					</Button>
+				</Modal.Footer>
+			</Modal>
 			<Navbar collapseOnSelect expand='lg' variant='dark'>
 				<Navbar.Brand href='/'>
 					<Image src={Brand} alt='Logo' className='logo' />{' '}
@@ -32,11 +64,13 @@ const NavbarComponent = () => {
 				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
 				<Navbar.Collapse id='responsive-navbar-nav'>
 					<Nav className='mr-auto'></Nav>
-					<LoginBtn variant='outline-light'>Login</LoginBtn>
+					<LoginBtn variant='outline-light' onClick={handleShow}>
+						Login
+					</LoginBtn>
 					<SignUpBtn variant='success'>Sign Up</SignUpBtn>
 				</Navbar.Collapse>
 			</Navbar>
-		</div>
+		</>
 	);
 };
 
